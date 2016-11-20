2fda37279bf5d2e126127d8288b83a58b2655003
aiporre <ariel.iporre.rivas@gmail.com>
2016-11-20 13:11:34 +0000

    trasient changes xbee 1

--- xbee_test.py
+++ xbee_test.py
@@ -3,9 +3,13 @@ import serial
 ser = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 string = 'Hello from Raspberry Pi'
 print 'Sending "%s"' % string
-ser.write('%s\n' % string)
+ser.open()
+# ser.write('%s\n' % string)
+try:
+   while True:
+       incoming = ser.readline().strip()
+       print 'Received %s' % incoming
+       # ser.write('RPi Received: %s\n' % incoming)
+except:
+   ser.close()
 
-while True:
-    incoming = ser.readline().strip()
-    print 'Received %s' % incoming
-    ser.write('RPi Received: %s\n' % incoming)
